0ac12d98ae270369f36c407fa8ae9e8d66b401a0
Guilherme Camillo <guilherme.camillo@veri.com.br>
2022-12-06 15:39:22 -0300

    fix: corrigindo arquivos

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Provider } from "react-native-paper";
 import Principal from "./src/pages";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <Provider>
-      <Principal />
+      <SafeAreaProvider>
+        <Principal />
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import axios from "axios";
-import { Text } from "react-native-paper";
+import {
+  Button,
+  Card,
+  Paragraph,
+  Avatar,
+  Badge,
+  Appbar,
+  ActivityIndicator,
+} from "react-native-paper";
+
+const url = "https://jsonplaceholder.typicode.com";
 
 const Principal = () => {
   const [albums, setAlbums] = useState<
     { userId: number; id: number; title: string }[]
   >([]);
 
+  const [loading, setLoading] = useState<boolean>(true);
+
   const getAllAlbums = () => {
-    axios.get(`https://jsonplaceholder.typicode.com/albums`).then((res) => {
-      const persons = res.data;
-      setAlbums(persons);
-    });
+    try {
+      axios.get(`${url}/albums`).then((res) => {
+        const persons = res.data;
+        setAlbums(persons);
+      });
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
@@ -20,13 +37,35 @@ const Principal = () => {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Principal</Text>
-      <FlatList
-        data={albums}
-        renderItem={({ item, index }) => <Text key={item.id}>{item.title}</Text>}
-      />
-    </View>
+    <>
+      <Appbar.Header>
+        <Appbar.Content title="Albums" />
+      </Appbar.Header>
+      <View style={styles.container}>
+        {loading && <ActivityIndicator animating={true} color="#1251C7" />}
+        <FlatList
+          data={albums}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item, index }) => (
+            <Card key={index} style={styles.card}>
+              <Card.Content>
+                <Avatar.Text size={24} label={String(item.userId)} />
+                <Badge>{item.id}</Badge>
+                <Paragraph>{item.title}</Paragraph>
+              </Card.Content>
+            </Card>
+          )}
+        />
+        <View style={styles.containerButtons}>
+          <Button mode="contained" onPress={() => setAlbums([])}>
+            Apagar Albums
+          </Button>
+          <Button mode="contained" onPress={() => getAllAlbums()}>
+            Gerar Albums
+          </Button>
+        </View>
+      </View>
+    </>
   );
 };
 
@@ -35,7 +74,17 @@ export default Principal;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#FFF",
+    padding: 16,
+  },
+  card: {
     backgroundColor: "#A3A3A3",
-    padding: 60,
+    marginBottom: 10,
+    borderRadius: 8,
+    padding: 16,
+  },
+  containerButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
